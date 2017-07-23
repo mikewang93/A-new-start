d2950ae9280d190dad41698b1b9e5940ed4ab7c9
mikewang93 <hard944766344@gmail.com>
2017-07-23 13:25:24 +0800

    Create StopWatch.py

---
+++ StopWatch.py
@@ -0,0 +1,13 @@
+import time
+while 1:
+  value = raw_input('Press Enter to start timer':)
+  if value == '':
+    startTime = time.time()
+    print startTime
+    pass
+  value1= raw_input('Press Enert to end the timer':)
+  if value1 == '':
+    endTime = time.time()
+    print endTime
+    totalTime = endTime - startTime
+    print 'The total Time is',totalTime
