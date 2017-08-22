0f51abb8f93ccface4fb00055dac3d9dcab39a07
mikewang93 <hard944766344@gmail.com>
2017-08-22 11:07:55 +0800

    Update Duotai.py

--- Duotai.py
+++ Duotai.py
@@ -12,3 +12,12 @@ class Cat(Animal):
 def run_twice(Animal):
   Animal.run()
   Animal.run()
+a = Animal()
+b = Dog()
+c = Cat()
+
+print "a is Animal?",isinctance(a,Animal)
+print "b is Dog?",isinstance(b,Dog)
+print "c is Cat?",isinstance(c,Cat)
+
+run_twice(b)
