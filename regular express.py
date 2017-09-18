9d7d07b6aa54630e3e19f88c5b9eca5e74fd1538
mikewang93 <hard944766344@gmail.com>
2017-09-18 22:30:17 +0800

    Update regular express.py

--- regular express.py
+++ regular express.py
@@ -2,3 +2,10 @@ import re
 
 # Try to match a ip address
 re.search(r'(([01]{0,1}\d{0,3}\d{0,3})\.){3}[01]{0,1}\d{0,3}\d{0,3}','192.168.1.1')
+#try to match some charactor
+
+re.search(r'[a-z]{4}','mike')
+
+#.+*？
+
+re.search(r'[01]\d\d|2[0-4]\d|25[0-5]','192')
